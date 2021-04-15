11e0b17f062a920d006c4dbbd5f42bb46f2159ef
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-04-15 16:20:17 +0200

    add progress bar to main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Text, Button, TextInput, View } from 'react-native';
-
+import React, { Component } from 'react';
+import { Text, Button, TextInput, View, StyleSheet } from 'react-native';
+// import { Constants } from 'expo';
 // import {ProgressBar} from "./components/progress-bar.component";
 
 const App = () => {
@@ -25,21 +25,53 @@ const App = () => {
         color='green'
         >
       </Button>
-      <ProgressBar />
+      {/* <ProgressBar /> */}
+      <View style={styles.progressBar}>
+        <View style={styles.filler} />
+      </View>
     </View>
   );
 }
 
-const ProgressBar = (props) => {
-  return (
-    <div className="progress-bar">
-      <Filler />
-    </div>
-  )
-}
+// const ProgressBar = (props) => {
+//   return (
+//     <div className="progress-bar">
+//       <Filler />
+//     </div>
+//   )
+// }
 
-const Filler = (props) => {
-  return <div className="filler" />
-}
+// const Filler = (props) => {
+//   return <div className="filler" />
+// }
+
+
+
+const styles = StyleSheet.create({
+  progressBar: {
+    // position: relative,
+    height: 20,
+    width: 350,
+    borderRadius: 50,
+    borderWidth: 2, 
+    borderColor: '#B9DEA4'
+  },
+  
+  filler: {
+    backgroundColor: '#CFCFCF',
+    height: 18,
+    borderRadius: 50
+  }
+})
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.progressBar}>
+//         <View style={styles.filler} />
+//       </View>
+//     );
+//   }
+// }
 
 export default App;

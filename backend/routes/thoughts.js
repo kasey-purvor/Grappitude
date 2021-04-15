d1aa86e8532fd75b91ca0e0979542702f0f81826
Kasey P <kaseyp@ma370.home>
2021-04-15 15:33:27 +0100

    CRUD backend fully implemented and manually tested.

--- backend/routes/thoughts.js
+++ backend/routes/thoughts.js
@@ -18,4 +18,27 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  Thought.findById(req.params.id)
+    .then(thought => res.json(thought))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Thought.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Thought deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Thought.findById(req.params.id)
+    .then(thought => {
+      thought.thought = req.body.thought;
+      thought.save()
+        .then(() => res.json('Thought updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;

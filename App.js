9e316320d1d11350b5fe83d11b08869aab7c8615
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-04-18 19:04:32 +0200

    create separate stylesheet for progress bar, can now interpolate the width of the filler

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Text, Button, TextInput, View, StyleSheet } from 'react-native';
 
 import CreateThought from "./components/create-thought.component";
-import ProgressBarExample from "./components/progress-bar.component";
+import ProgressBar from "./components/progress-bar.component";
 
 const App = () => {
   return (
     < View>
     <CreateThought />
-    <ProgressBarExample />
+    <ProgressBar />
     </View>
 
   )
--- components/progress-bar.component.js
+++ components/progress-bar.component.js
@@ -1,62 +1,96 @@
-import React,  { Component } from 'react';
-import { Text, Button, TextInput, View, StyleSheet } from 'react-native';
+// import React,  { Component } from 'react';
+// import { Text, Button, TextInput, View, StyleSheet } from 'react-native';
+// import axios from "axios";
+
+//  export default class ProgressBarExample extends Component {  
+//   constructor(props) {
+//     super(props)
+    
+//     this.state = {
+//       percentage: 50
+//     }
+    
+//     this.nextStep = this.nextStep.bind(this)
+//   }
+  
+//   nextStep() {
+//     if(this.state.percentage === 100) return 
+//     this.setState(prevState => ({ percentage: prevState.percentage + 20 }))
+//   }
+  
+//   render() {
+//     return (
+//       <div>
+        
+//         <h2> A React Progress Bar </h2>
+//         <ProgressBar percentage={this.state.percentage} />
+        
+//         <div style={{ marginTop: '20px' }}>  
+//           <button 
+//             onClick={this.nextStep}
+//            >
+//             Next Step
+//           </button>  
+//         </div>   
+        
+//         <div style={{marginTop: '10px', color: 'blue', marginBottom: '15px'}} onClick={() => this.setState({ percentage: 0 })}>
+//           Reset
+//         </div>
+//       </div>
+//     )
+//   }  
+// }
+
+// const ProgressBar = (props) => {
+//   return (
+//       <div className="progress-bar">
+//         <Filler percentage={props.percentage} />
+//       </div>
+//     )
+// }
+
+// const Filler = (props) => {
+//   return <div className="filler" style={{ width: `${props.percentage}%` }} />
+// }
+
+import React from 'react';
+import { View } from 'react-native';
+import styles from './progress-bar.component.style';
 import axios from "axios";
 
- export default class ProgressBarExample extends Component {  
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      percentage: 50
-    }
-    
-    this.nextStep = this.nextStep.bind(this)
-  }
-  
-  nextStep() {
-    if(this.state.percentage === 100) return 
-    this.setState(prevState => ({ percentage: prevState.percentage + 20 }))
-  }
-  
-  render() {
+const customStyle = {
+  width: '0%'
+}
+
+
+
+axios.get('http://localhost:5000/thoughts')
+.then((response) => {
+  const data = response.data;
+  console.log(data)
+  if (data.length === data.length ++) {
+    customStyle.width = `${50}%`
+  }})
+
+  customStyle.width = `${10}%`
+
+// was going to test out if can do data.length then increase width by 10, if length increments by 1. if length is multiple of 10, width goes back to zero.
+
+
+
+
+
+
+function ProgressBar() {
+
     return (
-      <div>
-        
-        <h2> A React Progress Bar </h2>
-        <ProgressBar percentage={this.state.percentage} />
-        
-        <div style={{ marginTop: '20px' }}>  
-          <button 
-            onClick={this.nextStep}
-           >
-            Next Step
-          </button>  
-        </div>   
-        
-        <div style={{marginTop: '10px', color: 'blue', marginBottom: '15px'}} onClick={() => this.setState({ percentage: 0 })}>
-          Reset
-        </div>
-      </div>
-    )
-  }  
-}
-
-const ProgressBar = (props) => {
-  return (
-      <div className="progress-bar">
-        <Filler percentage={props.percentage} />
-      </div>
-    )
-}
-
-const Filler = (props) => {
-  return <div className="filler" style={{ width: `${props.percentage}%` }} />
-}
 
 
+      // for some reason cant get both bar and filler to show 
+      <View style={[styles.filler, customStyle]}/>
+    )}
 
-
-
+export default ProgressBar
 
 // export default class ProgressBar extends Component {
 //  constructor(props) {

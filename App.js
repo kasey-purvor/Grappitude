1f6d1c226bcbceec1506eab4fc1f5c03e621849f
ianmcnicholas <ianjmcnicholas@.com>
2021-04-19 15:02:58 +0100

    User thoughts displayed in correct order, and inspire-me button established

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { Text, Button, TextInput, View, StyleSheet } from 'react-native';
 
 import CreateThought from "./components/create-thought.component";
+import InspireMe   from "./components/inspire-me.component";
 import DisplayThoughts from "./components/display-thoughts.component";
+
 
 const App = () => {
   return (
     <View>
 
       <CreateThought />,
+      <InspireMe />
       <DisplayThoughts />
 
     </View>

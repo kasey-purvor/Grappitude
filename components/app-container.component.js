96e09169e224eefae79202e189345f74da7ed063
Eds <steadyede@live.co.uk>
2021-04-21 17:44:52 +0100

    whole app scrollable solution

--- components/app-container.component.js
+++ components/app-container.component.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { FlatList, Text, Button, TextInput, View, StyleSheet, Image, ScrollView } from 'react-native';
+import { FlatList, Text, Button, TextInput, View, StyleSheet, Image, ScrollView, SafeAreaView } from 'react-native';
 import axios from "axios";
 
 import styles from './design.component.style';
@@ -42,18 +42,26 @@ const AppContainer = () => {
   
   return (
       <View>
-        {/* <Image source={require("../assets/header.png")} style={styles.image}/>
-          <Text style={styles.title}>
-           Grappitude
-          </Text>
-        <AboutMe />
-      
-          <CreateThought afterThoughtCreated={afterThoughtCreated}/>
-          <Level healthLevel={healthLevel}/> */}
-          <ProgressBar progressBarWidth={progressBarWidth}/>
-          <InspireMe />
+        <ScrollView>
+        <SafeAreaView>
+            <Image source={require("../assets/header.png")} style={styles.image}/>
+              <Text style={styles.title}>
+              Grappitude
+              </Text>
+            <AboutMe />
+          
+              <CreateThought afterThoughtCreated={afterThoughtCreated}/>
+            <ScrollView>
+              {/* <SafeAreaView> */}
+              <Level healthLevel={healthLevel}/>
+              <ProgressBar progressBarWidth={progressBarWidth}/>
+              <InspireMe />
 
-          <DisplayThoughts />
+              <DisplayThoughts />
+              {/* </SafeAreaView> */}
+            </ScrollView>
+          </SafeAreaView>
+          </ScrollView>
 
       </View>
   )

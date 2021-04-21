efc50f97b91fd7ca3a189bb5197fe9d0b6a2565d
ianmcnicholas <ianjmcnicholas@.com>
2021-04-21 18:55:12 +0100

    updated about

--- about.js
+++ about.js
@@ -4,7 +4,7 @@ This app encourages you to regularly enter your thoughts into a private journal.
 You are encouraged to enter a thought on a daily basis so that you can reflect on these at a later date. With particular focus on what you are grateful for.\n
 Thoughts entered are recorded and count towards your progress, which is shown in the progress bar.\n
 When the bar is filled up, your user level will increase. Each level requires 10 thoughts to progress further.\n
-When you reach level 100... just keep on being grateful :)\n
+When you reach level 100... just keep going!\n
 Are you grappling with what to post?  Hit the 'Inspire Me' button to see what others have thought!\n\n
 Happy Grappituding!\n\n
 Creators:\n`
